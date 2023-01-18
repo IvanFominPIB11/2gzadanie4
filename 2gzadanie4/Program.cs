982d116b2681f43685e40e2f54b59b5996accbac
PIBO0121038 Фомин Иван Константинович <PIBO0121038@sibupk.su>
2023-01-18 15:26:29 +0700

    Добавьте файлы проекта.

---
+++ 2gzadanie4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _2gzadanie4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number;
+            number = Int32.Parse(
+                Interaction.InputBox(
+                    "Введите целое число от 5 до 10")
+                );
+            MessageBox.Show((number >= 5 && number <= 10) ? "Число ввели правильно"
+                : "Число ввели не правильно");
+        }
+    }
+}
